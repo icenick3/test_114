ff30f2868731999146998faa91c1a3c3b998a107
kolya <kolyapadda@gmailc.com>
2023-08-28 19:27:36 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import TopBlock from "./components/TopBlock/TopBlock";
+import MiddleBlock from "./components/MidleBlock/MidleBlock";
+import BotBlock from "./components/BotBlock/BotBlock";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <TopBlock/>
+            <MiddleBlock/>
+            <BotBlock/>
+            <Footer/>
         </div>
     );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Header.css"
+import SideMenu from "../SideMenu/SideMenu";
 
 
 const Header = () => {
 
 
+    const scrollToMain = () => {
+        const mainElement = document.getElementById('main');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    const scrollToFutures = () => {
+        const mainElement = document.querySelector('.add-h2');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+    const scrollToOver = () => {
+        const mainElement = document.querySelector('.over-text');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    const scrollToSlick = () => {
+        const mainElement = document.querySelector('.slick-slider');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+    const scrollToDownload = () => {
+        const mainElement = document.querySelector('.download');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+    const scrollToForm = () => {
+        const mainElement = document.querySelector('.form');
+        if (mainElement) {
+            mainElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+const [activeSideMenu, setActiveSideMenu] = useState(false)
+
+    console.log(activeSideMenu)
     return (
         <header>
             <p className="logo">LOGO HERE!</p>
             <div className="menu">
-                <p>Main</p>
-                <p>About Us</p>
-                <p>Features</p>
-                <p>Screenshots</p>
-                <p>Process</p>
-                <p>Contacts</p>
+                <p onClick={()=> scrollToMain()}>Main</p>
+                <p onClick={()=> scrollToFutures()}>About Us</p>
+                <p onClick={()=> scrollToOver()}>Features</p>
+                <p onClick={()=> scrollToSlick()}>Screenshots</p>
+                <p onClick={()=> scrollToDownload()}>Process</p>
+                <p onClick={()=> scrollToForm()}>Contacts</p>
             </div>
+            <img className="mobile-menu" src="https://i.pinimg.com/originals/c5/a1/0a/c5a10a030878fb9cc71995a31c1f5d5e.png" alt="" onClick={()=> setActiveSideMenu(true)}/>
+            <SideMenu setActiveSideMenu={setActiveSideMenu} activeSideMenu={activeSideMenu} scrollToMain={scrollToMain} scrollToFutures={scrollToFutures}
+                      scrollToOver={scrollToOver} scrollToSlick={scrollToSlick} scrollToDownload={scrollToDownload} scrollToForm={scrollToForm}/>
         </header>
     );
 };
